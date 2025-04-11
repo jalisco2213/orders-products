7867795ba29d97f03d45cf21e08093b26e8bd33c
Maxim <119533893+jalisco2213@users.noreply.github.com>
2025-04-11 10:57:32 +0300

    Update Navigation.vue

--- src/Layouts/Navigation.vue
+++ src/Layouts/Navigation.vue
@@ -1,47 +1,93 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import Settings from '../components/svg/Settings.vue';
+import BurgerMenu from '@/components/svg/BurgerMenu.vue';
 
+const isOpen = ref(false);
 </script>
 
 <template>
-  <aside>
-    <div class="aside-content">
-      <div class="aside-top">
-        <div class="aside-photo">
-          <img src="/avatar.png" alt="">
+  <div>
+    <div class="menu-toggle" @click="isOpen = !isOpen">
+      <BurgerMenu />
+    </div>
 
-          <div class="aside-photo_settings">
-            <Settings />
+    <aside :class="{ open: isOpen }">
+      <div class="aside-content">
+        <div class="aside-top">
+          <div class="aside-photo">
+            <img src="/avatar.png" alt="">
+            <div class="aside-photo_settings">
+              <Settings />
+            </div>
           </div>
         </div>
-      </div>
 
-      <nav>
-        <li>
-          <RouterLink to="/">Приход</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/group">Группы</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="#">Продукты</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="#">Пользователи</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="#">Настройки</RouterLink>
-        </li>
-      </nav>
-    </div>
-  </aside>
+        <nav>
+          <li>
+            <RouterLink to="/">Приход</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/group">Группы</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="#">Продукты</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="#">Пользователи</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="#">Настройки</RouterLink>
+          </li>
+        </nav>
+      </div>
+    </aside>
+  </div>
 </template>
+
 
 <style lang="scss">
 aside {
   width: 250px;
   height: 91vh;
   box-shadow: 15px 0 15px rgba(0, 0, 0, 0.1);
+  transition: transform 0.3s ease-in-out;
+  background: #fff;
+  position: static;
+  top: 0;
+  left: 0;
+  z-index: 10;
+
+  @media (max-width: 768px) {
+    position: fixed;
+    transform: translateX(-100%);
+
+    &.open {
+      transform: translateX(0);
+    }
+  }
+}
+
+.menu-toggle {
+  display: none;
+  position: fixed;
+  top: 20px;
+  left: 20px;
+  z-index: 15;
+  background: #fff;
+  padding: 10px;
+  border-radius: 8px;
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+
+  @media (max-width: 768px) {
+    display: block;
+  }
+
+  svg {
+    width: 30px;
+    height: 30px;
+  }
 }
 
 .aside-content {
@@ -76,10 +122,8 @@ aside {
     align-items: center;
     transition: .5s ease;
 
-
     &:hover {
       background: #eee;
-      transition: .5s ease;
     }
   }
 }
@@ -88,5 +132,19 @@ nav {
   font-size: 20px;
   text-align: center;
   margin-top: 50px;
+
+  li {
+    margin-bottom: 20px;
+    list-style: none;
+
+    a {
+      text-decoration: none;
+      color: #333;
+
+      &:hover {
+        color: #007bff;
+      }
+    }
+  }
 }
 </style>
